12d999a896c13febb399932c6052af9eea050292
Tom Conway <drtomc@gmail.com>
2021-07-09 05:46:17 +0000

    add missing file

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,9 @@
 const path = require('path');
 
 module.exports = {
-  entry: './src/index.js',
+  entry: './src/sicil.js',
   output: {
-    filename: 'main.js',
+    filename: 'sicil.js',
     path: path.resolve(__dirname, 'dist'),
   },
 };
