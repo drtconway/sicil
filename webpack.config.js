cb69d3f27878e3108720466e0e03bb415b8d0fdc
Tom Conway <tom.conway@mcri.edu.au>
2021-10-18 13:59:14 +1100

    compatability change.

--- webpack.config.js
+++ webpack.config.js
@@ -5,6 +5,7 @@ module.exports = {
     library: 'sicil',
     libraryTarget: 'umd',
     filename: 'sicil.js',
+    globalObject: 'this',
     path: path.resolve(__dirname, 'dist'),
   },
   mode: 'development',
